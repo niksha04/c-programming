51f4b266de04d15c03aba26484cd118779e0e9e9
niksha04 <garnaraniksha6@gmail.comm>
2024-06-28 17:05:57 +0530

    c program

--- program.c
+++ program.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-int main(){
-    int result = 0;
-    int i = 1;
-    while(i<=10){
-        result = result + i;
-        i++;
-    }
-    printf("Sum of number from 1 to 10 : %d", result);
-}
+// #include <stdio.h>
+// int main(){
+//     int result = 0;
+//     int i = 1;
+//     while(i<=10){
+//         result = result + i;
+//         i++;
+//     }
+//     printf("Sum of number from 1 to 10 : %d", result);
+// }
   
 
 // 
@@ -36,8 +36,8 @@ int main(){
 //         i++;
 //     }
 //        printf("\n");
-//        return 0;
 // }
+
 
 
 
